5f9ca252ffdf58f877cc10af626906d21a0c557a
Toma Rosa <toma.rosa@ik.me>
2024-09-28 11:12:16 +0200

    subscribe from the template with pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 import { PokemonComponent } from './pokemon/pokemon/pokemon.component';
-import { RegionService } from './region/region.service';
+import { Region, RegionService } from './region/region.service';
 
 @Component({
   selector: 'al-root',
   standalone: true,
-  imports: [RouterOutlet, PokemonComponent],
+  imports: [CommonModule, RouterOutlet, PokemonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    this.regionService.getRegions().subscribe();
-  }
+  private regionService = inject(RegionService);
+  regions$: Observable<Region[]> = new Observable<Region[]>();
 
-  regionService = inject(RegionService);
+  ngOnInit(): void {
+    this.regions$ = this.regionService.getRegions();
+  }
 }
--- src/app/region/region.service.ts
+++ src/app/region/region.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { inject, Injectable, signal, WritableSignal } from '@angular/core';
+import { Observable } from 'rxjs';
 
-interface Region {
+export interface Region {
   nom: string;
   code: string;
 }
@@ -13,10 +13,9 @@ interface Region {
 export class RegionService {
   private URL = 'https://geo.api.gouv.fr/regions?fields=nom,code';
   private http = inject(HttpClient);
+  regions: WritableSignal<Region[]> = signal([]);
 
   getRegions(): Observable<Region[]> {
-    return this.http
-      .get<Region[]>(this.URL)
-      .pipe(tap((data) => console.log(data)));
+    return this.http.get<Region[]>(this.URL);
   }
 }
