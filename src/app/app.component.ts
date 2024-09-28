cac854f98b30d4e7443f6d4c8b5854c160fce536
Toma Rosa <toma.rosa@ik.me>
2024-09-28 18:53:33 +0200

    filter by region with reactive form control observable valueChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,31 @@
 import { CommonModule } from '@angular/common';
 import {
   Component,
-  effect,
   inject,
   OnDestroy,
   OnInit,
   signal,
   WritableSignal,
 } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Subscription, tap } from 'rxjs';
 import { DepartmentService } from './department/department.service';
 import { Region, RegionService } from './region/region.service';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'al-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class AppComponent implements OnInit, OnDestroy {
   private regionService = inject(RegionService);
   private departmentService = inject(DepartmentService);
   private regions$ = this.regionService.getRegions();
   private regionSubscription!: Subscription;
+
+  regionsFilterControl = new FormControl('');
 
   filterBy: WritableSignal<string> = signal('');
 
@@ -36,20 +37,27 @@ export class AppComponent implements OnInit, OnDestroy {
 
   regionDepartments = this.departmentService.regionDepartments;
 
-  filterEffect = effect(() => {
-    this.regions = this.regionsSignal().filter((region) =>
-      region.name.toLowerCase().startsWith(this.filterBy().toLowerCase())
-    );
-  });
-
   ngOnInit(): void {
     this.regionSubscription = this.regions$.subscribe(
-      (regions) => (this.regions = regions)
+      (data) => (this.regions = data)
     );
+
+    this.regionsFilterControl.valueChanges
+      .pipe(
+        tap((data) => {
+          this.regionsSignal.set(
+            this.regions.filter((region) =>
+              region.name
+                .toLowerCase()
+                .startsWith(data ? data.toLowerCase() : '')
+            )
+          );
+        })
+      )
+      .subscribe();
   }
 
   ngOnDestroy(): void {
     this.regionSubscription.unsubscribe();
-    this.filterEffect.destroy();
   }
 }
