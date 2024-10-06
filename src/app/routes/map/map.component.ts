89a8e44d3634b8946ce587827811be63d167ed67
Toma Rosa <toma.rosa@ik.me>
2024-10-06 11:18:10 +0200

    update layer opacity

--- src/app/routes/map/map.component.ts
+++ src/app/routes/map/map.component.ts
@@ -1,17 +1,15 @@
 import 'ol/ol.css';
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { Map as OLMap, View as OLView } from 'ol';
 import { Tile as TileLayer } from 'ol/layer';
 import { OSM } from 'ol/source';
 
 @Component({
   standalone: true,
-  template: '<div id="map">',
-  styles: `#map {
-    height: 100dvh;
-  }`,
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css',
 })
-export default class MapComponent implements OnInit {
+export default class MapComponent implements OnInit, OnDestroy {
   map = signal(new OLMap());
 
   ngOnInit(): void {
@@ -35,7 +33,18 @@ export default class MapComponent implements OnInit {
     );
   }
 
-  handleClick() {
-    console.log(this.map().getAllLayers());
+  handleOpacityChange(event: Event) {
+    event.preventDefault();
+    const target = event.target as HTMLInputElement;
+
+    const layers = this.map().getAllLayers();
+    if (layers.length) {
+      const osm = layers[0];
+      osm.setOpacity(Number.parseFloat(target.value));
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.map().dispose();
   }
 }
